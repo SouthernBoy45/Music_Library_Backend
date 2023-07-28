7aa225805cd93dc63097d452b9cab806a8d3b1dd
Colten Bowdoin <coltenbowdoin@gmail.com>
2023-07-28 12:16:56 -0500

    Made more progress on the setup stage. Reviewing course content

---
+++ MusicLibraryWebAPI/MusicLibraryWebAPI/Controllers/MusicLibrariesController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicLibraryWebAPI.Data;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MusicLibraryWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MusicLibrariesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MusicLibrariesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<MusicLibrariesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<MusicLibrariesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<MusicLibrariesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<MusicLibrariesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<MusicLibrariesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- MusicLibraryWebAPI/MusicLibraryWebAPI/Models/MusicLibrary.cs
+++ MusicLibraryWebAPI/MusicLibraryWebAPI/Models/MusicLibrary.cs
@@ -6,8 +6,10 @@ namespace MusicLibraryWebAPI.Models
     {
         [Key]
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Title { get; set; }
         public string Artist { get; set; }
+        public string Album { get; set; }
+        public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
     }
 }

342eab2af8c0ec7d08730a9bf66c430e8b9d08fe
Colten Bowdoin <coltenbowdoin@gmail.com>
2023-07-28 10:22:25 -0500

    Began project setup.

---
+++ MusicLibraryWebAPI/MusicLibraryWebAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicLibraryWebAPI.Models;
+
+namespace MusicLibraryWebAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<MusicLibrary> MusicLibraries { get; set; }
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ MusicLibraryWebAPI/MusicLibraryWebAPI/Models/MusicLibrary.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicLibraryWebAPI.Models
+{
+    public class MusicLibrary
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Artist { get; set; }
+        public string Genre { get; set; }
+    }
+}
